f1a057acdd41985594d1d043862009e698c3d0e3
Wiebken <126778886+lu-x23@users.noreply.github.com>
2023-07-20 12:43:02 +0200

    Feat: Adds function to generate 8 contact on load

--- linked-in-contacts/script.js
+++ linked-in-contacts/script.js
@@ -4,9 +4,66 @@ const contactCardsContainer = document.querySelector(
 );
 
 window.addEventListener("load", function (event) {
+  fetchDataFullSite();
+});
+
+function renderContact(contactName, contactPicture, contactConnections) {
+  const contactCard = contactCardsContainer.appendChild(
+    document.createElement("div")
+  );
+  contactCard.classList.add("contact-card");
+  const btnCloseCard = contactCard.appendChild(document.createElement("btn"));
+  btnCloseCard.innerText = "x";
+  btnCloseCard.addEventListener("click", function (event) {
+    deleteCard(btnCloseCard);
+  });
+
+  const ImgForProfilePic = contactCard.appendChild(
+    document.createElement("img")
+  );
+  ImgForProfilePic.setAttribute("src", contactPicture);
+  const pForName = contactCard.appendChild(document.createElement("p"));
+  const nameText = pForName.appendChild(document.createTextNode(contactName));
+  const pForMutualConnections = contactCard.appendChild(
+    document.createElement("p")
+  );
+  const mutualConnections = pForMutualConnections.appendChild(
+    document.createTextNode(contactConnections + " " + "mutual connections")
+  );
+  const btnConnect = contactCard.appendChild(document.createElement("button"));
+  btnConnect.innerText = "Connect";
+}
+
+function deleteCard(button, contactName, contactPicture, contactConnections) {
+  button.parentElement.remove();
+  fetchDataForOnePerson();
+}
+
+function fetchDataFullSite() {
   fetch("https://dummy-apis.netlify.app/api/contact-suggestions?count=8")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      renderAllCards(data);
     });
-});
+}
+function fetchDataForOnePerson() {
+  fetch("https://dummy-apis.netlify.app/api/contact-suggestions?count=1")
+    .then((response) => response.json())
+    .then((data) => {
+      renderOneCard(data);
+    });
+}
+function renderOneCard(data) {
+  const contactPicture = person.picture;
+  const contactName = person.name.first + " " + person.name.last;
+  const contactConnections = person.mutualConnections;
+  renderContact(contactName, contactPicture, contactConnections);
+}
+function renderAllCards(data) {
+  for (person of data) {
+    const contactPicture = person.picture;
+    const contactName = person.name.first + " " + person.name.last;
+    const contactConnections = person.mutualConnections;
+    renderContact(contactName, contactPicture, contactConnections);
+  }
+}
